775df8268d32e1eb4f611c99cecb0cd6cd2c8ec2
Naoto Takahashi <naoto0715charlie@gmail.com>
2025-06-07 20:59:05 +0900

    役職のModel作成

---
+++ nekokaburigame/nekokaburigame/Model/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  nekokaburigame
+//
+//  Created by 高橋直斗 on 2025/06/07.
+//
+
+import Foundation
+
+enum Role: String, CaseIterable {
+    case human = "人間"
+    case noracat = "野良猫"
+    case robcat = "泥棒猫"
+    case bosscat = "ボス猫"
+}
+
